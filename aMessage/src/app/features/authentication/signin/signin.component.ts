34f8044838c0a964f7623d7c0cc906d31eae1066
Daniil Kolodich <danik.kolodich@mail.ru>
2024-11-16 19:01:51 +0300

    more angular setup

---
+++ aMessage/src/app/features/authentication/signin/signin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in',
+  standalone: true,
+  imports: [],
+  templateUrl: './signin.component.html',
+  styleUrl: './signin.component.sass'
+})
+export class SignInComponent {
+
+}
---
+++ aMessage/src/app/features/authentication/signup/signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.sass'
+})
+export class SignUpComponent {
+
+}
