f1a00f086e4345d46c825040920fd6e21f11c12e
Daniil Kolodich <danik.kolodich@mail.ru>
2024-11-16 18:03:22 +0300

    angular project setup

---
+++ aMessage.Angular/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NgbModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.sass'
+})
+export class AppComponent {
+}
